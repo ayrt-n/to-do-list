a78cef1ac0b81699919387fbb0d71a0ad2409d91
ayrton <ayrton.parkinson1@gmail.com>
2022-08-18 10:39:31 -0700

    Add method to read and change status

--- src/toDo.js
+++ src/toDo.js
@@ -1,7 +1,17 @@
 const todo = (title, description, dueDate, priority) => {
+  let status = 0;
+
+  const getStatus = () => {
+    return status;
+  };
+
+  const toggleStatus = () => {
+    status = status === 0 ? 1 : 0;
+  };
+
   // Return todo proprties
   // Status of zero is incomplete, status of one is complete
-  return { title, description, dueDate, priority, status: 0 };
+  return { title, description, dueDate, priority, getStatus, toggleStatus };
 }
 
 export default todo;

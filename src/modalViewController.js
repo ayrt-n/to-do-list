9548c90c41ac0b0c4484c196b8a16366057d3c39
ayrton <ayrton.parkinson1@gmail.com>
2022-08-21 17:38:34 -0700

    Add methods to construct new todo list modal

--- src/modalViewController.js
+++ src/modalViewController.js
@@ -3,22 +3,115 @@ const modalCard = document.getElementsByClassName('modal-card')[0];
 const modalContent = document.getElementsByClassName('modal-content')[0];
 const closeModalBtn = document.getElementById('close-modal');
 
-const viewTodoModal = (todo) => {
+const displayTodoModal = (todo) => {
   modalContent.innerHTML = '';
-  createTodoViewModal(todo);
+  _createTodoModal(todo);
   modal.style.display = 'block';
 };
 
-// Create more details modal and append to modal content div
-const createTodoViewModal = (todo) => {
+const displayNewTodoModal = () => {
   modalContent.innerHTML = '';
+  _createNewTodoModal();
+  modal.style.display = 'block';
+};
 
+const _createTitleField = () => {
+  const titleDiv = document.createElement('div');
+  titleDiv.classList.add('field');
+
+  const titleLabel = document.createElement('label');
+  titleLabel.classList.add('label');
+  titleLabel.setAttribute('for', 'title');
+  titleLabel.innerHTML = 'Title';
+  const titleInput = document.createElement('input');
+  titleInput.id = 'title';
+  titleInput.setAttribute('type', 'text');
+  titleInput.classList.add('input')
+
+  titleDiv.appendChild(titleLabel);
+  titleDiv.appendChild(titleInput);
+
+  return titleDiv;
+};
+
+const _createPriorityField = () => {
+  const priorityDiv = document.createElement('div');
+  priorityDiv.classList.add('field');
+  const priorityOptions = ['Low', 'Medium', 'High'];
+  const priorityLabel = document.createElement('label');
+  priorityLabel.setAttribute('for', 'priority');
+  priorityLabel.classList.add('label');
+  priorityLabel.innerHTML = 'Priority';
+  const priorityInput = document.createElement('select');
+  priorityInput.id = 'priority';
+  priorityInput.classList.add('select');
+  for (let i = 0; i < priorityOptions.length; i++) {
+    const priorityOption = document.createElement('option');
+    priorityOption.value = priorityOptions[i];
+    priorityOption.text = priorityOptions[i];
+    priorityInput.appendChild(priorityOption);
+  }
+
+  priorityDiv.appendChild(priorityLabel);
+  priorityDiv.appendChild(priorityInput);
+
+  return priorityDiv;
+};
+
+const _createDueDateField = () => {
+  const dueDateDiv = document.createElement('div');
+  dueDateDiv.classList.add('field');
+  const dueDateLabel = document.createElement('label');
+  dueDateLabel.setAttribute('for', 'duedate');
+  dueDateLabel.classList.add('label');
+  dueDateLabel.innerHTML = 'Due date';
+  const dueDateInput = document.createElement('input');
+  dueDateInput.id = 'duedate';
+  dueDateInput.setAttribute('type', 'date');
+
+  dueDateDiv.appendChild(dueDateLabel);
+  dueDateDiv.appendChild(dueDateInput);
+
+  return dueDateDiv;
+};
+
+const _createDescriptionField = () => {
+  const descriptionDiv = document.createElement('div');
+  descriptionDiv.classList.add('field');
+  const descriptionLabel = document.createElement('label');
+  descriptionLabel.setAttribute('for', 'description');
+  descriptionLabel.classList.add('label');
+  descriptionLabel.innerHTML = 'Description';
+  const descriptionInput = document.createElement('textarea');
+  descriptionInput.id = 'description';
+  descriptionInput.classList.add('textarea');
+
+  descriptionDiv.appendChild(descriptionLabel);
+  descriptionDiv.appendChild(descriptionInput);
+
+  return descriptionDiv;
+};
+
+const _createNewTodoModal = () => {
+  const todoTitle = _createTitleField();
+  const todoPriority = _createPriorityField();
+  const todoDueDate = _createDueDateField();
+  const todoDescription = _createDescriptionField();
+
+  modalContent.appendChild(todoTitle);
+  modalContent.appendChild(todoPriority);
+  modalContent.appendChild(todoDueDate);
+  modalContent.appendChild(todoDescription);
+};
+
+// Create more details modal and append to modal content div
+const _createTodoModal = (todo) => {
   const header = document.createElement('h1');
   header.innerHTML = todo.title
 
-  const priority = createModalTextSection('Priority', todo.priority);
-  const dueDate = createModalTextSection('Due date', todo.dueDate);
-  const description = createModalTextSection('Description', todo.description);
+  const priority = _createModalTextSection('Priority', todo.priority);
+  const dueDate = _createModalTextSection('Due date', todo.dueDate);
+  const description = _createModalTextSection('Description', todo.description);
 
   modalContent.appendChild(header);
   modalContent.appendChild(priority);
@@ -27,7 +120,7 @@ const createTodoViewModal = (todo) => {
 };
 
 // Create a standard modal text element with title and text content
-const createModalTextSection = (title, content) => {
+const _createModalTextSection = (title, content) => {
   const div = document.createElement('div');
 
   const sectionTitle = document.createElement('p');
@@ -55,4 +148,7 @@ closeModalBtn.onclick = () => {
   modal.style.display = 'none';
 }
 
-export default viewTodoModal;
+export {
+  displayTodoModal,
+  displayNewTodoModal,
+};

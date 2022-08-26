16ca657698651db6723be2574e10f888cd1aafc0
ayrton <ayrton.parkinson1@gmail.com>
2022-08-26 10:54:32 -0700

    Add method to dynamically load menu with projects

--- src/menuController.js
+++ src/menuController.js
@@ -1,3 +1,8 @@
+import inboxSVG from './images/inbox.svg';
+import calendarSVG from './images/inbox.svg';
+import chevronSVG from './images/chevron-down.svg';
+import circleSVG from './images/circle.svg';
+
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu');
 
@@ -5,4 +10,41 @@ burger.addEventListener('click', () => {
   menu.classList.toggle('is-hidden');
 });
 
-export default burger;
+const loadMenu = (projectsObject) => {
+  console.log('hi');
+  const inboxBtn = createMenuButton('Inbox', inboxSVG);
+  inboxBtn.classList.add('selected');
+
+  const projectsDiv = document.createElement('div');
+  projectsDiv.classList.add('projects');
+  const projectsBtn = createMenuButton('Projects', chevronSVG);
+  projectsDiv.appendChild(projectsBtn);
+  
+  for (let project in projectsObject) {
+    const projectBtn = createMenuButton(projectsObject[project].name, circleSVG);
+    projectBtn.classList.add('sub-item');
+    projectsDiv.appendChild(projectBtn);
+  }
+
+  menu.appendChild(inboxBtn);
+  menu.appendChild(projectsDiv);
+};
+
+const createMenuButton = (text, icon) => {
+  const menuItem = document.createElement('div');
+  menuItem.classList.add('menu-item');
+  const menuIcon = document.createElement('img');
+  menuIcon.src = icon;
+  menuIcon.classList.add('small-icon');
+  const menuText = document.createElement('p');
+  menuText.innerHTML = text;
+
+  menuItem.appendChild(menuIcon);
+  menuItem.appendChild(menuText);
+
+  return menuItem;
+};
+
+export {
+  loadMenu
+};

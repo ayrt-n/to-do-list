70cc63943cf7d4629dd29241dcc1f52fd523f272
ayrton <ayrton.parkinson1@gmail.com>
2022-08-26 10:01:14 -0700

    Add ability to save and load projects object from localStorage

--- src/index.js
+++ src/index.js
@@ -1,35 +1,16 @@
-import todo from "./todo";
+import Todo from "./todo";
 import project from "./project";
 import burger from "./menuController";
 import { loadProject, reloadProject } from "./loadTodos";
 import { displayTodoModal, displayNewTodoModal } from "./modalViewController";
 import { toggleTodoButton, removeTodoItem } from "./todoViewController";
+import { loadProjectsFromLocalStorage, setObject, getObject } from "./localStorage";
 
-const inbox = project('Inbox');
-const testTodo1 = todo('A dynamically generated task', 'N/a', 'Whenever', 'High');
-const testTodo2 = todo('A dynamically generated task', 'N/a', 'Whenever', 'High');
-const testTodo3 = todo('A dynamically generated task', 'N/a', 'Whenever', 'High');
-inbox.addTodo(testTodo1);
-inbox.addTodo(testTodo2);
-inbox.addTodo(testTodo3);
+// Load projects from local storage or generates new projects object
+const projects = loadProjectsFromLocalStorage();
 
-inbox.toggleTodoStatus(2);
-
-
-loadProject(inbox);
-
-const work = project('Work');
-const work1 = todo('A dynamically generated work task', 'N/a', 'Whenever', 'High');
-const work2 = todo('A dynamically generated work task', 'N/a', 'Whenever', 'High');
-work.addTodo(work1);
-work.addTodo(work2);
-
-loadProject(work);
-
-const projects = {
-  inbox,
-  work
-}
+// By default load the inbox project 
+loadProject(projects['inbox']);
 
 // Main content div contains todo lists and related functionality
 const mainContentDiv = document.getElementById('content');
@@ -44,14 +25,15 @@ mainContentDiv.addEventListener('click', (e) => {
 
   // Event delegation to determine which task to run
   if (e.target.matches('.radio')) {
-    toggleTodoButton(e.target);
+    toggleTodoButton(e.target); 
     projects[projectName].toggleTodoStatus(todoIndex);
+    setObject('projects', projects); // Save projects object
   } else if (e.target.matches('.title')) {
     displayTodoModal(todoItem);
   } else if (e.target.matches('.delete')) {
     projects[projectName].removeTodo(todoIndex);
-    console.log(projects[projectName]);
     reloadProject(projects[projectName]);
+    setObject('projects', projects); // Save projects object
   } else if (e.target.matches('.add-todo')) {
     displayNewTodoModal(projectName);
   }
@@ -70,11 +52,12 @@ modalContentDiv.addEventListener('click', (e) => {
   const dueDateValue = document.getElementById('duedate').value;
   const descriptionValue = document.getElementById('description').value;
   const projectValue = document.getElementById('project').value;
-  const todoItem = todo(titleValue, descriptionValue, dueDateValue, priorityValue);
+  const todoItem = new Todo(titleValue, descriptionValue, dueDateValue, priorityValue);
 
   modal.style.display = 'none';
 
   // Add todo item to project todo list and refresh DOM
   projects[projectValue].addTodo(todoItem);
   reloadProject(projects[projectValue]);
+  setObject('projects', projects); // Save projects object
 });

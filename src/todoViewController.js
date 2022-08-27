19f110edbdca638b92757012a5a477f16c5c28e8
ayrton <ayrton.parkinson1@gmail.com>
2022-08-27 09:54:57 -0400

    Combine loadTodos and todoViewController

--- src/todoViewController.js
+++ src/todoViewController.js
@@ -1,14 +1,144 @@
+import trashSvg from './images/garbage.svg';
+
+// Module handles manipulating main content div which holds projects and todo lists
+const contentDiv = document.getElementById('content');
+
 // Toggle radio button styling to select/deselect
 const toggleTodoButton = (target) => {
   target.classList.toggle('selected');
 };
 
-// Remove todo from todo list
-const removeTodoItem = (target) => {
-  target.parentElement.remove();
+// Clear main content div
+const clearProjects = () => {
+  contentDiv.innerHTML = '';
+};
+
+// Create and display multiple projects given project object
+const loadProjects = (projectsObject) => {
+  
+};
+
+// Create and display a project in the main content div
+const loadProject = (project) => {
+  const header = document.createElement('h1');
+  header.innerHTML = project.name;
+  const todosDiv = document.createElement('div');
+  todosDiv.classList.add('todos');
+  todosDiv.appendChild(header);
+  todosDiv.id = project.name.toLowerCase();
+  
+  project.todoList.forEach((todo, index) => {
+    const todoListItem = createTodoListItem(todo, project, index);
+    todosDiv.appendChild(todoListItem);
+  });
+
+  const addTask = createAddTaskButton(project);
+
+  todosDiv.appendChild(addTask);
+  contentDiv.appendChild(todosDiv);
+};
+
+// Clear DOM and rebuild the given project in its containing div
+const reloadProject = (project) => {
+  const projectDiv = document.getElementById(project.name.toLowerCase());
+  projectDiv.innerHTML = '';
+
+  const header = document.createElement('h1');
+  header.innerHTML = project.name;
+  projectDiv.appendChild(header);
+
+  project.todoList.forEach((todo, index) => {
+    const todoListItem = createTodoListItem(todo, project, index);
+    projectDiv.appendChild(todoListItem);
+  });
+
+  const addTask = createAddTaskButton(project);
+  projectDiv.appendChild(addTask);
+};
+
+// Create radio button element for todo list item
+const createRadioButton = (todo, project, index) => {
+  const radioDiv = document.createElement('div');
+  radioDiv.classList.add('radio');
+  radioDiv.setAttribute('todo-index', index);
+  radioDiv.setAttribute('project', project.name.toLowerCase());
+  
+  if (todo.status === 1) {
+    radioDiv.classList.add('selected');
+  }
+
+  return radioDiv;
+};
+
+// Create div to display details of the todo list item
+const createTodoItem = (todo, project, index) => {
+  const projectDetails = document.createElement('div');
+  projectDetails.classList.add('project-details');
+
+  const title = document.createElement('div');
+  title.classList.add('title');
+  title.innerHTML = todo.title;
+  title.setAttribute('project', project.name.toLowerCase());
+  title.setAttribute('todo-index', index);
+
+  const dueDate = document.createElement('div');
+  dueDate.classList.add('due-date', 'sub-title');
+  dueDate.innerHTML = todo.dueDate;
+
+  projectDetails.appendChild(title);
+  projectDetails.appendChild(dueDate);
+
+  return projectDetails;
+};
+
+// Create trash icon for todo list item
+const createTrashIcon = (project, index) => {
+  const trashIcon = document.createElement('img');
+  trashIcon.src = trashSvg;
+  trashIcon.classList.add('small-icon', 'delete');
+  trashIcon.setAttribute('todo-index', index);
+  trashIcon.setAttribute('project', project.name.toLowerCase());
+  
+  return trashIcon;
+};
+
+// Create the complete todo list element for the DOM
+const createTodoListItem = (todo, project, index) => {
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
+  const radioButton = createRadioButton(todo, project, index);
+  const trashIcon = createTrashIcon(project, index);
+  const todoDetails = createTodoItem(todo, project, index);
+
+  todoDiv.appendChild(radioButton);
+  todoDiv.appendChild(todoDetails);
+  todoDiv.appendChild(trashIcon);
+
+  return todoDiv;
+}
+
+// Create the add task button
+const createAddTaskButton = (project) => {
+  const addTodoDiv = document.createElement('div');
+  addTodoDiv.classList.add('add-todo');
+  addTodoDiv.setAttribute('project', project.name.toLowerCase());
+
+  const plusDiv = document.createElement('div');
+  plusDiv.classList.add('plus');
+  const plus = document.createTextNode('+')
+  plusDiv.appendChild(plus);
+
+  addTodoDiv.appendChild(plusDiv);
+  const taskTextNode = document.createTextNode('Add task')
+  addTodoDiv.appendChild(taskTextNode);
+
+  return addTodoDiv;
 };
 
 export {
-  toggleTodoButton,
-  removeTodoItem
+  loadProject,
+  reloadProject,
+  clearProjects,
+  toggleTodoButton
 }
